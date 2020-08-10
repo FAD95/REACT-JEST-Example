e1e5c4595d20a59b6037577de44dd7c6d63eeba4
Fadith Escorcia <feescorciam@protonmail.com>
2020-08-09 21:25:33 -0500

    Prueba implementada

--- src/__test__/Hello.test.js
+++ src/__test__/Hello.test.js
@@ -6,4 +6,11 @@ import Adapter from 'enzyme-adapter-react-16'
 // Para que Enzyme entienda que trabajara con react
 Enzyme.configure({ adapter: new Adapter() })
 
-
+describe('Tests React', () => {
+  test('Check the text in the props', () => {
+    const data = { title: 'Hello Jest' }
+    const wrapper = mount(<Hello title={data.title} />)
+    const h1 = wrapper.find('h1')
+    expect(h1.text()).toBe('Hello Jest')
+  })
+})
